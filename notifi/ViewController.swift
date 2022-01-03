45d1a85fcb874d0352aadeeeaaaf257744f0b26e
INGWANG LEE <93109525+leegg666@users.noreply.github.com>
2022-01-03 18:03:43 +0900

    추가 alert

--- notifi/ViewController.swift
+++ notifi/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var createPopUpBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBAction func onCreatePopUpBtnClicked(_ sender: UIButton) {
+        let storyboard = UIStoryboard.init(name: "PopUp", bundle: nil)
+        let alertPopUpVC = storyboard.instantiateViewController(identifier: "AlertPopUpVC") as AlertPopUpViewController
+        alertPopUpVC.modalPresentationStyle = .overCurrentContext
+        alertPopUpVC.modalTransitionStyle = .crossDissolve
+    
+        alertPopUpVC.subscribeBtnClickedCompletionClosure = {
+            print("컴플레션 블럭이 호출 되었다.")
+            
+            let scannedUrl = URL(string: "https://www.youtube.com/channel/UCutO2H_AVmWHbzvE92rpxjA?view_as=subscriber")
+            self.myWebView.load(URLRequest(url: scannedUrl!))
+               
+        }
+    
+        self.present(alertPopUpVC, animated: true, completion: nil)
+    }
+    
+    
 }
 
